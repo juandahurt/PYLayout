b1f87a7ab9ab4184aa05a2fde5fd6d00a02d6ba0
Juan David Hurtado <juandahurtadom@gmail.com>
2022-05-13 14:12:19 -0500

    chore: set up demo project

--- Tests/PYLayoutTests/PYLayoutTests.swift
+++ Tests/PYLayoutTests/PYLayoutTests.swift
@@ -6,6 +6,6 @@ final class PYLayoutTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PYLayout().text, "Hello, World!")
+//        XCTAssertEqual(PYLayout().text, "Hello, World!")
     }
 }
