19f522b3a24449c1ba03cdc90d0ad46aaabbb68f
Juan David Hurtado <juandahurtadom@gmail.com>
2022-05-13 20:15:46 -0500

    feat: add text color to text view

--- Sources/PYLayout/SwiftUI/Views/Text/PYTextView.swift
+++ Sources/PYLayout/SwiftUI/Views/Text/PYTextView.swift
@@ -10,14 +10,17 @@ import SwiftUI
 public struct PYTextView: View {
     let content: String
     let fontSize: Int
+    let textColor: Color
     
-    public init(_ content: String, fontSize: Int) {
+    public init(_ content: String, fontSize: Int, textColor: Color = .black) {
         self.content = content
         self.fontSize = fontSize
+        self.textColor = textColor
     }
     
     public var body: some View {
         Text(content)
             .font(.system(size: CGFloat(fontSize)))
+            .foregroundColor(textColor)
     }
 }
