39b9d9fdefc77de94a8357f01cc1816ee13d2a67
Juan David Hurtado <juandahurtadom@gmail.com>
2022-05-27 14:45:34 -0500

    feat: add offsetable scroll view

---
+++ Sources/Purace/Views/Utils/Offsetable Scroll View/OffsettableScrollView.swift
@@ -0,0 +1,50 @@
+//
+//  OffsettableScrollView.swift
+//  
+//
+//  Created by Juan Hurtado on 27/05/22.
+//
+
+import Foundation
+import SwiftUI
+
+public struct OffsettableScrollView<T: View>: View {
+    let axes: Axis.Set
+    let showsIndicator: Bool
+    let onOffsetChanged: (CGPoint) -> Void
+    let content: T
+    
+    public init(axes: Axis.Set = .vertical,
+         showsIndicator: Bool = true,
+         onOffsetChanged: @escaping (CGPoint) -> Void = { _ in },
+         @ViewBuilder content: () -> T
+    ) {
+        self.axes = axes
+        self.showsIndicator = showsIndicator
+        self.onOffsetChanged = onOffsetChanged
+        self.content = content()
+    }
+    
+    public var body: some View {
+        ScrollView(axes, showsIndicators: showsIndicator) {
+            GeometryReader { proxy in
+                Color.clear.preference(
+                    key: OffsetPreferenceKey.self,
+                    value: proxy.frame(
+                        in: .named("ScrollViewOrigin")
+                    ).origin
+                )
+            }
+            .frame(width: 0, height: 0)
+            content
+        }
+        .coordinateSpace(name: "ScrollViewOrigin")
+        .onPreferenceChange(OffsetPreferenceKey.self,
+                            perform: onOffsetChanged)
+    }
+}
+
+private struct OffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGPoint = .zero
+    static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) { }
+}
