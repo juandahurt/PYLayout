bc23d44fba99c77d18d53e296bea5f2a15ed6c39
Juan David Hurtado <juandahurtadom@gmail.com>
2022-06-11 13:15:42 -0500

    refactor: update image skeleton

--- Sources/Purace/Views/Basic/Image/PuraceImageView.swift
+++ Sources/Purace/Views/Basic/Image/PuraceImageView.swift
@@ -8,23 +8,45 @@
 import Foundation
 import SwiftUI
 import Kingfisher
+import ViewExtractor
 
 public struct PuraceImageView: View {
     let skeletonColor: Color
     let url: URL?
+    let overlayViews: [AnyView]
+    
+    @State var hasLoaded = false
     
     public init(url: URL?) {
-        self.skeletonColor = PuraceStyle.Color.allSkeletons.randomElement()!
+        skeletonColor = PuraceStyle.Color.allSkeletons.randomElement()!
         self.url = url
+        overlayViews = []
+    }
+    
+    public init<Content: View>(url: URL?, @ViewBuilder content: NormalContent<Content>) {
+        skeletonColor = PuraceStyle.Color.allSkeletons.randomElement()!
+        self.url = url
+        overlayViews = ViewExtractor.getViews(from: content)
     }
     
     public var body: some View {
-        KFImage.url(url)
-            .resizable()
-            .appendProcessor(DownsamplingImageProcessor(size: UIScreen.main.bounds.size))
-            .scaleFactor(UIScreen.main.scale)
-            .placeholder { _ in
-                skeletonColor
-            }
+        ZStack {
+            KFImage.url(url)
+                .resizable()
+                .fade(duration: 0.2)
+                .appendProcessor(DownsamplingImageProcessor(size: UIScreen.main.bounds.size))
+                .scaleFactor(UIScreen.main.scale)
+                .placeholder { _ in
+                    skeletonColor
+                }
+                .onSuccess { _ in
+                    hasLoaded = true
+                }
+            Group {
+                ForEach(overlayViews.indices) { index in
+                    overlayViews[index]
+                }
+            }.opacity(hasLoaded ? 1: 0)
+        }
     }
 }
