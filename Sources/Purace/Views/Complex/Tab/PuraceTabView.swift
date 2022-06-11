1ebff147a46f472608f7a4b2529db9cd217e0f97
Juan David Hurtado <juandahurtadom@gmail.com>
2022-06-11 11:08:46 -0500

    fix: tab tap gesture fixed

--- Sources/Purace/Views/Complex/Tab/PuraceTabView.swift
+++ Sources/Purace/Views/Complex/Tab/PuraceTabView.swift
@@ -24,6 +24,7 @@ public struct PuraceTabView<T: View>: View {
             ForEach(0..<titles.count) { index in
                 PuraceTextView(titles[index], fontSize: 12, textColor: selectedIndex == index ? PuraceStyle.Color.N1 : PuraceStyle.Color.N4, weight: selectedIndex == index ? .medium : .regular)
                     .frame(width: size.width / CGFloat(titles.count))
+                    .background(Color.gray.opacity(0.001))
                     .onTapGesture {
                         selectedIndex = index
                         updateIndicatorOffset(size: size)
