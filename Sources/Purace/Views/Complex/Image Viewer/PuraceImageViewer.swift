a555395d4917f2698cd732d9bd3976df05679cf2
Juan David Hurtado <juandahurtadom@gmail.com>
2022-06-25 11:29:19 -0500

    feat: add drag offset

--- Sources/Purace/Views/Complex/Image Viewer/PuraceImageViewer.swift
+++ Sources/Purace/Views/Complex/Image Viewer/PuraceImageViewer.swift
@@ -13,6 +13,9 @@ public struct PuraceImageViewer: View {
     let backgroundColor: Color
     let url: URL?
     @State var opacity: Double = 0
+    @State var dragOffset: CGFloat = .zero
+    @State var backgroundOpacity: Double = 1
+    @State var hasDroppedTheImage = false
     
     public init(url: URL?, isVisible: Binding<Bool>) {
         let colors: [Color] = [
@@ -29,10 +32,30 @@ public struct PuraceImageViewer: View {
     public var body: some View {
         ZStack {
             backgroundColor
+                .opacity(backgroundOpacity)
             PuraceImageView(url: url)
                 .scaledToFit()
                 .frame(maxHeight: UIScreen.main.bounds.height * 0.75)
-                .animation(.none)
+                .offset(x: 0, y: dragOffset)
+                .animation(hasDroppedTheImage ? .easeOut(duration: 0.35) : .none)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            let translation = value.translation.height
+                            dragOffset = translation
+                            backgroundOpacity = 1 - abs(translation) * 0.001
+                        }
+                        .onEnded { value in
+                            withAnimation {
+                                backgroundOpacity = 1
+                            }
+                            hasDroppedTheImage = true
+                            dragOffset = .zero
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.35) {
+                                hasDroppedTheImage = false
+                            }
+                        }
+                )
         }
         .edgesIgnoringSafeArea(.all)
         .animation(.easeIn)
