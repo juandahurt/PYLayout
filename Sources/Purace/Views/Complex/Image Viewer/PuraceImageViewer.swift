ae1d6f8d1bb932f7d54537e700277127f3a38ace
Juan David Hurtado <juandahurtadom@gmail.com>
2022-06-26 13:01:24 -0500

    fix: fix zoom gesture

--- Sources/Purace/Views/Complex/Image Viewer/PuraceImageViewer.swift
+++ Sources/Purace/Views/Complex/Image Viewer/PuraceImageViewer.swift
@@ -13,11 +13,17 @@ public struct PuraceImageViewer: View {
     let backgroundColor: Color
     let url: URL?
     @State var opacity: Double = 0
-    @State var dragOffset: CGFloat = .zero
+    @State var dragOffset: CGSize = .zero
     @State var backgroundOpacity: Double = 1
     @State var hasDroppedTheImage = false
     @State var dragInitialTime: Date?
-    @State var scale: CGFloat = 1 // zoom
+    
+    @State var currentScale: CGFloat = 1
+    @State var finalScale: CGFloat = 1 // zoom
+    
+    @State var dragAccumulator: CGSize = .zero
+    @State var imageSize: CGSize = .zero
+    private let maximumImageHeight = UIScreen.main.bounds.height * 0.65
     
     public init(url: URL?, isVisible: Binding<Bool>) {
         let colors: [Color] = [
@@ -38,56 +44,99 @@ public struct PuraceImageViewer: View {
         return date.timeIntervalSince(dragInitialTime) * 1000
     }
     
-    var image: some View {
-        PuraceImageView(url: url)
-            .scaledToFit()
-            .frame(maxHeight: UIScreen.main.bounds.height * 0.75)
-            .offset(x: 0, y: dragOffset)
-            .animation(hasDroppedTheImage ? .easeOut(duration: 0.35) : .none)
-            .scaleEffect(scale)
+    var draggableArea: some View {
+        Color.black
+            .opacity(0.001)
             .gesture(
                 DragGesture()
                     .onChanged { value in
-                        if dragInitialTime == nil {
-                            dragInitialTime = Date()
+                        if currentScale == 1 {
+                            if dragInitialTime == nil {
+                                dragInitialTime = Date()
+                            }
+                            let translation = value.translation.height
+                            dragOffset.height = translation
+                            backgroundOpacity = 1 - abs(translation) * 0.001
+                        } else {
+//                            dragOffset.width += value.translation.width + dragAccumulator.width
+//                            dragOffset.width *= 1/currentScale
+//                            dragOffset.height += value.translation.height + dragAccumulator.width
+//                            dragOffset.height *= 1/currentScale
                         }
-                        let translation = value.translation.height
-                        dragOffset = translation
-                        backgroundOpacity = 1 - abs(translation) * 0.001
                     }
                     .onEnded { value in
-                        let diff = differenceBeetwenInitialDragTime(and: Date())
-                        if diff <= 150 {
-                            let translation = value.translation.height
-                            withAnimation {
-                                let screenHeight = UIScreen.main.bounds.height * 0.65
-                                dragOffset = translation > 0 ? screenHeight : -screenHeight
+                        if currentScale == 1 {
+                            let diff = differenceBeetwenInitialDragTime(and: Date())
+                            if diff <= 150 {
+                                let translation = value.translation.height
+                                withAnimation {
+                                    let screenHeight = UIScreen.main.bounds.height * 0.65
+                                    dragOffset.height = translation > 0 ? screenHeight : -screenHeight
+                                }
+                                isVisible = false
+                            } else {
+                                withAnimation {
+                                    backgroundOpacity = 1
+                                }
+                                hasDroppedTheImage = true
+                                dragOffset = .zero
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.35) {
+                                    hasDroppedTheImage = false
+                                }
                             }
-                            isVisible = false
+                            dragInitialTime = nil
                         } else {
-                            withAnimation {
-                                backgroundOpacity = 1
-                            }
-                            hasDroppedTheImage = true
-                            dragOffset = .zero
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.35) {
-                                hasDroppedTheImage = false
-                            }
+                            dragAccumulator.width += value.translation.width
+                            dragAccumulator.height += value.translation.height
                         }
-                        dragInitialTime = nil
                     }
             )
             .simultaneousGesture(
                 MagnificationGesture()
                     .onChanged { value in
-                        scale = value
+                        guard value < 3.5 else { return }
+                        let aux = max(0.6, value)
+                        if value > 1 {
+                            if aux < finalScale {
+                                currentScale = finalScale
+                            } else {
+                                currentScale = aux
+                            }
+                        } else {
+                            currentScale = aux
+                        }
                     }
                     .onEnded { value in
                         if value < 1 {
-                            scale = 1
+                            currentScale = 1
+                            finalScale = 1
+                        } else {
+                            finalScale = currentScale
                         }
                     }
             )
+    }
+    
+    var image: some View {
+        PuraceImageView(url: url)
+            .scaledToFit()
+            .offset(x: dragOffset.width, y: dragOffset.height)
+            .animation(hasDroppedTheImage ? .easeOut(duration: 0.35) : .none)
+            .scaleEffect(currentScale)
+            .overlay(
+                GeometryReader { reader in
+                    Color.black.opacity(0.001)
+                        .onAppear {
+                            updateImageSize(from: reader.size)
+                        }
+                }
+            )
+        .frame(maxHeight: maximumImageHeight)
+    }
+    
+    func updateImageSize(from size: CGSize) {
+        imageSize = size
+        print(size)
     }
     
     public var body: some View {
@@ -95,12 +144,14 @@ public struct PuraceImageViewer: View {
             backgroundColor
                 .opacity(backgroundOpacity)
             image
+            draggableArea
         }
         .edgesIgnoringSafeArea(.all)
         .animation(.easeIn)
         .onChange(of: $isVisible.wrappedValue) { value in
             if value {
-                dragOffset = 0
+                dragOffset = .zero
+                dragAccumulator = .zero
                 backgroundOpacity = 1
                 withAnimation {
                     opacity = 1
